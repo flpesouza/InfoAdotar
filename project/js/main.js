876d3a5f91217283fd25f7445a9e5cd0c1f7c803
Felipe Tertuliano <11701331@aluno.cotemig.com.br>
2020-06-13 20:53:22 -0300

    Criando layout do projeto

--- project/js/main.js
+++ project/js/main.js
@@ -1,18 +1,5 @@
-$(document).ready(function () {
-    menuToggleScript();
-});
-
-const menu = "#menu";
-const menuToggle = "#menu-toggle";
-const menuProfile = "#menu-profile";
-const menuNav = "#menu-nav";
-const notificationsSummary = "#notifications-summary";
-
-
-//#region #menu-toggle
-function menuToggleScript() {
-    $(menuToggle).click(function () {
-        $(`${menu} > *:not(${menuToggle})`).toggle();
+$(document).ready(function() {
+    $("#pageHeader > .menu-toggle").click(function () {
+        $('#menu').animate({width: 'toggle'});
     });
-}
-//#endregion
+})
